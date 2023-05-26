476e9c22d24f5bff7c42bdaf408489ad8005730e
Tophy <tophyrakotomavo@gmail.com>
2023-05-26 16:33:26 +0300

    Finish

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,57 +1,123 @@
-import React from "react"
+"use client"
+import React, { useState } from "react";
+import { sendEmail } from "@/server/contact";
+import HeaderContact from "@/components/HeaderContact";
+import Footer from "@/components/Footer";
 
-const page = () => {
-  return(
-    <div className="flex justify-center items-center h-screen ">
-      <div className="sm:w-2/5 md:h-auto h-fit md:border-solid sm:border-slate-950 sm:border-2 sm:shadow-lg rounded-lg">
-        <div className="pl-3">
-          <h1 className="text-2xl">Contactez-nous</h1>
-          <span className="">Un probleme, une question, envie de nous envoyer un message?</span>
-        </div>
-        <div className="p-6">
-          <div className="form-control w-2/4">
-            <label className="label">
-              <span className="label-text font-bold">Your Name</span>
-            </label>
-            <input type="text" 
-              placeholder="Type here" 
-              className="input input-sm w-full" 
-            />
-          </div>
-          <div className="form-control w-2/4">
-            <label className="label">
-              <span className="label-text font-bold">Your Adress E-mail</span>
-            </label>
-            <input type="text" 
-              placeholder="Type here" 
-              className="input input-sm w-full" 
-            />
-          </div>
-          <div className="form-control w-2/4">
-            <label className="label">
-              <span className="label-text font-bold">What is your message type?</span>
-            </label>
-            <div className="form-control">
-              <select className="select input select-sm select-bordered text-slate-600">
-                <option disabled selected>dsdsdsds</option>
-                <option>T-shirts</option>
-                <option>Mugs</option>
-              </select>
+const formatEmail = /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+const Page = () => {
+  const [obj, setObj] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+  const [message, setMessage] = useState('');
+  const [isloading, setIsLoading] = useState(false);
+  
+  const handleSend = async () => {
+    setIsLoading(true)
+    const Obj = await sendEmail({
+      name,
+      email,
+      obj,
+      message,
+    });
+    
+    if(Obj){
+      setSuccess(true), setIsLoading(false)
+    }
+    else{setError("error")}
+    
+  };
+  
+  return (
+    <div>
+      <HeaderContact />
+      <div className="flex justify-center w-fit absolute">
+        {success && 
+          <div className="alert alert-success shadow-lg w-fit">
+            <div>
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              <p>Votre message a été envoyé avec succès !</p>
             </div>
           </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text font-bold">Your message</span>
-            </label>
-            <textarea placeholder="Bio" className="textarea textarea-md w-full max-w-xs rounded-lg p-2" ></textarea>
+        }
+      </div>
+      {error && 
+        <div className="alert alert-error shadow-lg w-fit absolute">
+          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <p>{error}</p>
+        </div>
+      }
+      <div className="flex justify-center items-center flex-col h-screen">
+        <b className="flex justify-center pb-10">
+          <span id="txtContact" className="font-bold sm:text-4xl text-xl">Contact</span>
+        </b>
+        <div className="sm:w-96 md:h-auto h-fit shadow-md shadow-slate-400 rounded-lg bg-white">
+          <div className="p-6">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-bold">Nom</span>
+              </label>
+              <input type="text"
+                placeholder="Type here"
+                className="input input-sm w-full"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-bold">
+                  Adress E-mail
+                </span>
+              </label>
+              <input type="text"
+                placeholder="Type here"
+                className="input input-sm w-full"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-bold">Objets</span>
+              </label>
+              <input type="text"
+                placeholder="Type here"
+                className="input input-sm w-full"
+                value={obj}
+                onChange={(e) => setObj(e.target.value)}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-bold">Votre message</span>
+              </label>
+              <textarea placeholder="Bio"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                className="textarea textarea-md w-full max-w-xs rounded-lg p-2">
+              </textarea>
+            </div>
+          </div>
+          <div className="grid sm:place-content-end p-6">
+            <button type="submit" className="btn btn-outline btn-xs sm:btn-sm md:btn-md"
+              disabled={!formatEmail.test(email) || !name || !obj || !message || isloading}
+              onClick={ handleSend }
+            >
+              { isloading? "Loading...": "Envoyer mon message"}
+            </button>
           </div>
         </div>
-        <div className="grid sm:place-content-end p-6">
-          <button className="btn btn-outline btn-xs sm:btn-sm md:btn-md">Send my message</button>
-        </div>
       </div>
+      <Footer />
     </div>
   );
 };
 
-export default page;
+export default Page;
--- src/app/galeries/page.tsx
+++ src/app/galeries/page.tsx
@@ -2,18 +2,49 @@
 import React, { useState, useCallback } from "react";
 import ImageViewer from "react-simple-image-viewer";
 import Image from "next/image";
-import HeaderImg from "@/components/HeaderImg";
 import Footer from "@/components/Footer";
-import Pict from "@/image/raisin-sante.jpg"
-import img from '@/image/Artisanal-malgache.png';
-import Img from '@/image/Rafia-bag.png'
-import Smailing from "@/image/smiling-busi.jpg"
+import HeaderGalerie from "@/components/HeaderGalerie";
+
+import Deco from "@/image/imageGalerie/Deco.jpg";
+import Deco1 from "@/image/imageGalerie/Deco_avec_sac.jpg";
+import Deco2 from "@/image/imageGalerie/Deco_avec_sac1.jpg";
+import Deco3 from "@/image/imageGalerie/Deco_carre.jpg";
+import Plaque from "@/image/imageGalerie/Plaque_The.jpg";
+import Deco4 from "@/image/imageGalerie/Deco_et_porte_couteau.jpg";
+import LaCroix from "@/image/imageGalerie/La_croix.jpg";
+import LaCroix1 from "@/image/imageGalerie/La_croix_l_marron.jpg";
+import LaCroix2 from "@/image/imageGalerie/La_croix_l_marron1.jpg";
+import LaCroix3 from "@/image/imageGalerie/La_croix_mini.jpg";
+import SacTsia from "@/image/imageGalerie/Sac_tsia.jpg";;
+import TroisSac from "@/image/imageGalerie/trois_Sac_de_meme_couleur.jpg";
+import SacTsia1 from "@/image/imageGalerie/Sac_tsia1.jpg";
+import SacTsia2 from "@/image/imageGalerie/Poche_sac_tsia.jpg";
+import Structure from "@/image/imageGalerie/Structure_en_bois.jpg";
+import Strcture1 from "@/image/imageGalerie/Structure_en_bois_vue_de_face.jpg";
+import Structure2 from "@/image/imageGalerie/Structure_en_bois_vue_profil.jpg";
+import Structure3 from "@/image/imageGalerie/Structure_en_bois_vue_profil1.jpg";
+import Structure4 from "@/image/imageGalerie/Structure_en_bois_vue_profilf.jpg"
+import Bondoliere from "@/image/imageGalerie/Bondoliere_tsia.jpg";
+import Bondoliere1 from "@/image/imageGalerie/Bondoliere_tsia_vue_profil.jpg";
+import TroisTasses from "@/image/imageGalerie/Trois_tasses.jpg";
+import TroisSacDiff from "@/image/imageGalerie/trois_Sac_en_couleur_different.jpg";
+import BondoliereCuire from "@/image/imageGalerie/Bondoliere_vue_de_dessus.jpg";
+import BondoliereCuire1 from "@/image/imageGalerie/Bondoliere_vue_de_face.jpg";
+import BondoliereCuire2 from "@/image/imageGalerie/Bondoliere_vue_priofil.jpg";
+import Plaque from "@/image/imageGalerie/Plaque_The.jpg";
 
 const imagesArray = [
-  img.src, Img.src, Smailing.src, Img.src, Pict.src, Img.src, Img.src, Smailing.src, Img.src
+  BondoliereCuire.src, BondoliereCuire1.src, BondoliereCuire2.src,
+  Bondoliere.src, Bondoliere1.src,
+  Deco.src, Deco1.src, Deco2.src, Deco3.src, Deco4.src,
+  LaCroix.src, LaCroix1.src, LaCroix2.src, LaCroix3.src,
+  SacTsia.src, SacTsia1.src, SacTsia2.src,
+  Strcture1.src, Structure.src, Structure2.src, Structure3.src, Structure4.src,
+  TroisSac.src, TroisTasses.src, TroisSacDiff.src,
+  Plaque.src,
 ];
 
-const Test = () => {
+const Page = () => {
   const [currentImage, setCurrentImage] = useState(0);
   const [isViewerOpen, setIsViewerOpen] = useState(false);
 
@@ -30,11 +61,11 @@ const Test = () => {
   return(
     <>
       <div>
-        <HeaderImg/>
+        <HeaderGalerie/>
         <div className="text-center my-8">
           <span id="txtGalerie" className="font-bold sm:text-4xl text-xl">Galeries</span>
         </div>
-        <div className="columns-3 gap-3 m-auto w-5/6">
+        <div className="columns-5 gap-3 m-auto w-5/6">
           {imagesArray.map((src, index) => (
             <Image
               src={src}
@@ -66,4 +97,4 @@ const Test = () => {
   );
 };
 
-export default Test;
+export default Page;
