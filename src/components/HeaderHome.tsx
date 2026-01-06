fb7022b61c4b9d7c7f8317f173268f669b3642b0
Tophy Lad <tophyrakotomavo@gmail.com>
2026-01-06 19:15:44 +0300

    Fix: menu positioning and text formatting

--- src/components/HeaderHome.tsx
+++ src/components/HeaderHome.tsx
@@ -3,20 +3,22 @@ import Navbar from "./Navbar";
 import Menus from "./Menus";
 
 const HeaderHome = () => {
-  
-  return(
-    <div >
-      <Navbar/>
-      <div id="headerHome" className='sm:h-screen h-80'>
-        <div className="justify-end hidden sm:flex">
-          <Menus/>  
-        </div>
+  return (
+    <div>
+      <Navbar />
+      <div className="justify-end hidden sm:flex sticky top-2">
+        <Menus />
+      </div>
+      <div id="headerHome" className="sm:h-screen h-80">
         <h1 className="font-serif text-white text-center sm:text-5xl text-2xl">
           OOAY
         </h1>
         <div className="content-center md:my-52 my-14 pb-10 p-2 md:pb-36">
           <div className="font-serif md:text-5xl text-2xl text-center text-white">
-            Venez découvrir nos matières premières<br/>d&apos;exception pour sublimer vos créations<br/> culinaires.
+            Venez découvrir nos matières premières
+            <br />
+            d&apos;exception pour sublimer vos créations
+            <br /> culinaires.
           </div>
         </div>
       </div>
@@ -25,4 +27,3 @@ const HeaderHome = () => {
 };
 
 export default HeaderHome;
-
--- src/components/Menus.tsx
+++ src/components/Menus.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
  
 const Menus = () => (
-  <div className="flex justify-evenly w-64 absolute text-white text-xs pt-2">
+  <div className="flex justify-evenly w-64 text-white text-xs pt-2">
     <Link href="#Saveur" >Saveurs</Link>
     <Link href="#Produits" >Produits</Link>
     <Link href="#Biographie">Biographie</Link>
