67a23ca922a19345de45a418dfe7ae4770bffbd2
Tophy Lad <tophyrakotomavo@gmail.com>
2026-01-06 20:14:19 +0300

    wip

--- src/app/api/test/route.ts
+++
@@ -1,7 +0,0 @@
-import { type NextRequest } from 'next/server'
-
-export const GET = (req: NextRequest) => {
-  const searchParams = req.nextUrl.searchParams
-  const query = searchParams.get('value')
-  return Response.json({ hello: 'word', value: query});
-};
